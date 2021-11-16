9a5f9addbc8dcd406f6e715e9f1bf51ca84dffc1
Zoe Roux <zoe.roux@sdg.moe>
2021-11-16 16:55:26 +0100

    Creating jobs to build images

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM jenkins/jenkins:lts
 ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
 COPY jenkins/plugins.txt /usr/share/jenkins/ref/plugins.txt
 COPY jenkins .
+COPY images images
 ENV CASC_JENKINS_CONFIG ./config.yml
 ENV ADMIN_PASSWORD password
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -6,3 +6,54 @@ folder("Projects") {
 	description("The available projets in whanos.")
 }
 
+languages = ["c", "java", "javascript", "python", "befunge"]
+
+languages.each { language ->
+	freeStyleJob("Whanos base images/whanos-$language") {
+		steps {
+			shell("docker build images/$language/Dockerfile.base")
+		}
+	}
+}
+
+freeStyleJob("Whanos base images/Build all base images") {
+	publishers {
+		downstream(
+			languages.collect { language -> "Whanos base images/whanos-$language" }
+		)
+	}
+}
+
+freeStyleJob("link-project") {
+	parameters {
+		stringParam("GITHUB_NAME", null, 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+		stringParam("DISPLAY_NAME", null, "Display name for the job")
+	}
+	steps {
+		dsl {
+			text('''
+				freeStyleJob("Projects/$DISPLAY_NAME") {
+					triggers {
+						scm("M/1")
+					}
+					scm {
+						git {
+							remote {
+								name("origin")
+								github("$GITHUB_NAME")
+							}
+						}
+					}
+					wrappers {
+						steps {
+							shell("make fclean")
+							shell("make")
+							shell("make tests_run")
+							shell("make clean")
+						}
+					}
+				}
+			''')
+		}
+	}
+}
